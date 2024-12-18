c8237ae5675c9368eb7641230c6371641c5b6214
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2024-12-18 14:40:30 -0600

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from "./styles/components/_button.module.scss";
+
+function App() {
+	return (
+		<>
+			<h1>TEST</h1>
+			<button className={styles.primary}>click here</button>
+			<button className={styles.secondary}>click here</button>
+		</>
+	);
+}
+
+export default App;
